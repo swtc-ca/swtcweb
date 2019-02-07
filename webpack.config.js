45073958927c024cb0b8463a3dafa432784fdacf
Xinchun Liu <xcliu@ca.ibm.com>
2019-02-07 10:44:36 -0500

    default webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path=require('path')
+
+module.exports = {
+	mode: 'development',
+	//entry: './src',
+	//output: {
+	//	path: path.resolve(__dirname, 'dist'),
+	//	//filename: 'bundle.js',
+	//	//publicPath: '/assets/',
+	//	//library: 'MyLibrary',
+	//	//libraryTarget: 'umd',
+	//},
+	//context: __dirname,
+	//module: {},
+	//resolve: {},
+	//performance: {},
+	//target: 'web',
+	//externals: [],
+	//stats: "errors-only",
+	//devServer: {},
+	//plugins: []
+}
